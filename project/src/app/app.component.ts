20fe643b930b9bbbf1ac8cbdd596e927464f36f5
Kevin Liao <kevin.liao@mail.aircast.biz>
2018-09-28 13:38:42 +0800

    Adding Navigtion with event binding and ngIf

--- project/src/app/app.component.ts
+++ project/src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'project';
+  navSelectedValue: string = 'recipes';
+
+  onNavSelected(navSelected: string) {
+    this.navSelectedValue = navSelected;
+  }
 }
